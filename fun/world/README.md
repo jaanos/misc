a6b8c9678aa1e9e9b875329dccbfaa0cc2164261
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2015-03-03 22:28:11 +0100

    Added world.

---
+++ fun/world/README.md
@@ -0,0 +1,4 @@
+World
+=====
+
+Some functions for drawing geodesics on a world map.
---
+++ fun/world/world.r
@@ -0,0 +1,27 @@
+source("uvozi.zemljevid.r")
+
+bisector.th <- function(ph1, th1, ph2, th2) {
+  cth1 <- cospi(th1/180)
+  cth2 <- cospi(th2/180)
+  sth <- sinpi(th2/180) - sinpi(th1/180)
+  return(function(ph) {
+    180*atan((cospi((ph1-ph)/180)*cth1 - cospi((ph2-ph)/180)*cth2)/sth)/pi
+  })
+}
+
+capitals <- read.csv2("capitals.csv", row.names = 1)
+
+svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip",
+                             "ne_110m_admin_0_countries", encoding = "Windows-1252")
+
+divide.world <- function(c1, c2, xlim=c(-180, 180), ylim=c(-90, 90), add = FALSE,
+                         col = "red", col1 = "blue", col2 = "green", cex = 1) {
+  if (!add) {
+    plot(svet, xlim=xlim, ylim=ylim)
+  }
+  curve(bisector.th(capitals[c1,"Longitude"], capitals[c1,"Latitude"],
+                    capitals[c2,"Longitude"], capitals[c2,"Latitude"])(x),
+        add = TRUE, col = "red")
+  points(capitals[c(c1, c2), c("Longitude", "Latitude")],
+         pch = 15, col = c(col1, col2), cex = cex)
+}
